74ef5333ce853d7acfdc54d7074d3028e094c4f7
퉁이리 <wer4272@nate.com>
2023-02-14 17:56:00 +0900

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import type { AppProps } from 'next/app';
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>next-ts-boilerplate</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>next-ts-boilerplate</main>
+    </>
+  );
+}
