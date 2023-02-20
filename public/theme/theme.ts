6aa51bd3a47f244232a483129cfd769d5dbe6a6c
Sunghum Paik <zerosial@gmail.com>
2023-02-20 16:46:04 +0900

    chore: 차크라 UI 커스텀 색상 추가

---
+++ public/theme/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const heyTheme = extendTheme({
+  colors: {
+    hey: {
+      main: '#F96400',
+      sub: '#F7B500',
+      darkGray: '#777777',
+      lingtGray: '#D9D9D9',
+      red: '#E53E3E',
+      lightOrange: '#FEEBCB',
+    },
+  },
+});
