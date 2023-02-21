fa3f8b0b5449cb8dae5a081e573a6849d0b2f16a
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-02-22 00:10:01 +0900

    refactor: Head content 올바른 위치로 재이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { heyTheme } from '@/public/theme/theme';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
 
@@ -25,11 +25,21 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, []);
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={heyTheme}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Hey, cake</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script
+          defer
+          src="https://developers.kakao.com/sdk/js/kakao.js"
+        ></script>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={heyTheme}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </>
   );
 }
