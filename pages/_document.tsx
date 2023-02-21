16018ebb2576c12599e461f01888c37fbb114f2d
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-02-21 22:59:00 +0900

    refactor: Head content 올바른 위치로 이동

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>Hey, cake</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
