3746a57d451ad0660a5cf6eaee836846c85d9c0e
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-02-22 00:10:01 +0900

    refactor: Head content 올바른 위치로 재이동

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
 
@@ -25,11 +26,21 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, []);
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={heyTheme}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools />
-      </ChakraProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Hey, cake</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script
+          defer
+          src="https://developers.kakao.com/sdk/js/kakao.js"
+        ></script>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={heyTheme}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools />
+        </ChakraProvider>
+      </QueryClientProvider>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,14 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <title>Hey, cake</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script
-          defer
-          src="https://developers.kakao.com/sdk/js/kakao.js"
-        ></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
