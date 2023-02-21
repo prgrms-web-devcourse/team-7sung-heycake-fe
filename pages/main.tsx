101fe7ffbd6baf6fdbddec4cc5dd5a8b2af91ec6
Sunghum Paik <zerosial@gmail.com>
2023-02-21 19:43:08 +0900

    feat: main으로 주소 이동

---
+++ pages/main.tsx
@@ -0,0 +1,38 @@
+import Header from '@/components/Header';
+import DataTabs from '@/components/Main/cakeTabs';
+
+export default function Home() {
+  const tabData = [
+    {
+      label: '전체',
+      content: '전체 케이크',
+    },
+    {
+      label: '생크림',
+      content: '생크림 케이크',
+    },
+    {
+      label: '쵸코',
+      content: '초콜릿 케이크',
+    },
+    {
+      label: '전체',
+      content: '전체 케이크',
+    },
+    {
+      label: '생크림',
+      content: '생크림 케이크',
+    },
+    {
+      label: '쵸코',
+      content: '초콜릿 케이크',
+    },
+  ];
+
+  return (
+    <>
+      <Header />
+      <DataTabs data={tabData} />
+    </>
+  );
+}
