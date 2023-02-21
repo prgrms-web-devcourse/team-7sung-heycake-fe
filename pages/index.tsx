bdc78270706bbe035e1dd2ccf11e5efbfd78dc5b
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-02-21 15:26:53 +0900

    feat: 로그인/비회원 진입 버튼 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,40 @@
 import Header from '@/components/Header';
 import Head from 'next/head';
+import Image from 'next/image';
+import { Button } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Head>
-        <title>next-ts-boilerplate</title>
+        <title>Hey, cake</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main>next-ts-boilerplate</main>
+      <ImageContainer>
+        <Image src="/images/logo.png" alt="로고" width={300} height={300} />
+      </ImageContainer>
+      <ButtonContainer>
+        <Button bg="#F7B500" width="20rem" margin="1rem">
+          카카오톡으로 로그인
+        </Button>
+        <Button bg="#FFEAB2" width="20rem">
+          비회원으로 둘러보기
+        </Button>
+      </ButtonContainer>
     </>
   );
 }
+
+const ImageContainer = styled.main`
+  display: flex;
+  justify-content: center;
+  margin-top: 7rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 3rem;
+`;
