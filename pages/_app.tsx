2fa8158ba4f3456c1807828a3d1a2cd6ccfbc514
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-02-21 23:09:08 +0900

    feat: kakao init 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,28 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 import { heyTheme } from '@/public/theme/theme';
 
 const queryClient = new QueryClient();
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    try {
+      if (!window.Kakao.isInitialized() && window.Kakao) {
+        window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_KEY);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={heyTheme}>
