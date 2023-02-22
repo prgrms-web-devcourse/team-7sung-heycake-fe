187da1dc1de8165abb05da4f786e28d5c96f827b
tooooo1 <wer4272@nate.com>
2023-02-22 20:27:33 +0900

    feat: datatable component

---
+++ pages/orders/components/DataTable.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+
+interface BorderData {
+  title: string;
+  value: string;
+  bottom?: 'bottom';
+}
+
+const DataTable = ({ title, value, bottom }: BorderData) => {
+  return (
+    <Wrapper id={bottom}>
+      <Title>{title}</Title>
+      <div>{value}</div>
+    </Wrapper>
+  );
+};
+
+export default DataTable;
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.2rem 0;
+  border-bottom: 1px solid #c7c7c7;
+  font-size: 0.8rem;
+
+  &#bottom {
+    border-bottom: 1px solid #c7c7c7;
+  }
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+`;
