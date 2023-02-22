cb5868483f14a10c5fb2f2ce5cb36bd800507aca
Sunghum Paik <zerosial@gmail.com>
2023-02-22 18:13:15 +0900

    refactor: 병합 일부 수정

--- pages/market/[id].tsx
+++ pages/market/[id].tsx
@@ -1,10 +1,8 @@
-import Header from '@/components/Header';
 import MarketProfile from '@/components/Market/marketProfile';
 
 export default function Market() {
   return (
     <>
-      <Header />
       <MarketProfile />
     </>
   );
