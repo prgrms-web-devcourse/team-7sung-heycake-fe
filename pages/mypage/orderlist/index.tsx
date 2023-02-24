142bd503b20341e7433fa0847cdb6ef19d1a1567
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-02-24 16:47:42 +0900

    feat: 내 주문 리스트 페이지 구현

--- pages/mypage/orderlist/index.tsx
+++ pages/mypage/orderlist/index.tsx
@@ -1,3 +1,17 @@
+import { Container } from '@chakra-ui/react';
+
+import MypageTitle from '@/components/Mypage/mypageTitle';
+import Post from '@/components/Mypage/post';
+
 export default function Orderlist() {
-  return <div>Orderlist</div>;
+  return (
+    <>
+      <MypageTitle title="내 주문 리스트" />
+      <Container marginTop={10}>
+        <Post />
+        <Post />
+        <Post />
+      </Container>
+    </>
+  );
 }
