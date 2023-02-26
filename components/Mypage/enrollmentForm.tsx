b096190327545326510f00127ea4ad354b94fb82
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-02-26 21:45:15 +0900

    feat: validation 추가

--- components/Mypage/enrollmentForm.tsx
+++ components/Mypage/enrollmentForm.tsx
@@ -1,7 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import { FormControl, FormLabel, Input, Select } from '@chakra-ui/react';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+} from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import { SubmitHandler, useForm } from 'react-hook-form';
+
+import ERROR_MESSAGES from '@/constants/errorMessages';
+
+const { CHECK_BUSINESS_NUMBER, CHECK_OWNER_NAME } = ERROR_MESSAGES;
 
 type Inputs = {
   businessNumber: string;
@@ -22,105 +32,145 @@ export default function EnrollmentForm() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormControl>
+      <FormControl height={100} width={350}>
         <FormLabel>사업자 등록 번호</FormLabel>
-        <Input required type="text" {...register('businessNumber')} />
+        <Input
+          type="text"
+          {...register('businessNumber', {
+            required: true,
+            minLength: 10,
+            maxLength: 10,
+          })}
+        />
+        {errors.businessNumber && (
+          <ErrorSpan>{CHECK_BUSINESS_NUMBER}</ErrorSpan>
+        )}
       </FormControl>
-      <FormControl>
+      <FormControl height={100} width={350}>
         <FormLabel>대표자 이름</FormLabel>
-        <Input required type="text" {...register('ownerName')} />
+        <Input
+          type="text"
+          {...register('ownerName', {
+            required: true,
+            minLength: 2,
+            maxLength: 20,
+          })}
+        />
+        {errors.ownerName && <ErrorSpan>{CHECK_OWNER_NAME}</ErrorSpan>}
       </FormControl>
-      <FormControl>
+      <FormControl height={100} width={350}>
         <FormLabel>개업 일자</FormLabel>
-        <Input required type="text" {...register('openDate')} />
+        <Input type="date" {...register('openDate', { required: true })} />
       </FormControl>
-      <FormControl>
+      <FormControl height={100} width={350}>
         <FormLabel>상호명</FormLabel>
-        <Input required type="text" {...register('phoneNumber')} />
+        <Input type="text" {...register('phoneNumber', { required: true })} />
       </FormControl>
-      <FormControl>
+      <FormControl height={150} width={350}>
         <FormLabel>주소</FormLabel>
         <AddressDiv>
-          <Select required {...register('city')} width={100}>
-            <option selected value="">
-              시 선택
-            </option>
-            <option value="seoul">서울시</option>
+          <Select
+            {...register('city', { required: true })}
+            width={170}
+            marginRight={2}
+            marginBottom={2}
+            defaultValue=""
+          >
+            <option value="">시 선택</option>
+            <option value="서울시">서울시</option>
           </Select>
-          <Select required {...register('district')} width={100}>
-            <option selected value="">
-              구 선택
-            </option>
-            <option value="seoul">강남구</option>
-            <option value="seoul">강동구</option>
-            <option value="seoul">강서구</option>
-            <option value="seoul">강북구</option>
-            <option value="seoul">관악구</option>
-            <option value="seoul">광진구</option>
-            <option value="seoul">구로구</option>
-            <option value="seoul">금천구</option>
-            <option value="seoul">노원구</option>
-            <option value="seoul">동대문구</option>
-            <option value="seoul">도봉구</option>
-            <option value="seoul">동작구</option>
-            <option value="seoul">마포구</option>
-            <option value="seoul">서대문구</option>
-            <option value="seoul">성동구</option>
-            <option value="seoul">성북구</option>
-            <option value="seoul">서초구</option>
-            <option value="seoul">송파구</option>
-            <option value="seoul">영등포구</option>
-            <option value="seoul">용산구</option>
-            <option value="seoul">양천구</option>
-            <option value="seoul">은평구</option>
-            <option value="seoul">종로구</option>
-            <option value="seoul">중구</option>
-            <option value="seoul">중랑구</option>
+          <Select
+            defaultValue=""
+            {...register('district', { required: true })}
+            width={170}
+          >
+            <option value="">구 선택</option>
+            <option value="강남구">강남구</option>
+            <option value="강동구">강동구</option>
+            <option value="강서구">강서구</option>
+            <option value="강북구">강북구</option>
+            <option value="관악구">관악구</option>
+            <option value="광진구">광진구</option>
+            <option value="구로구">구로구</option>
+            <option value="금천구">금천구</option>
+            <option value="노원구">노원구</option>
+            <option value="동대문구">동대문구</option>
+            <option value="도봉구">도봉구</option>
+            <option value="동작구">동작구</option>
+            <option value="마포구">마포구</option>
+            <option value="서대문구">서대문구</option>
+            <option value="성동구">성동구</option>
+            <option value="성북구">성북구</option>
+            <option value="서초구">서초구</option>
+            <option value="송파구">송파구</option>
+            <option value="영등포구">영등포구</option>
+            <option value="용산구">용산구</option>
+            <option value="양천구">양천구</option>
+            <option value="은평구">은평구</option>
+            <option value="종로구">종로구</option>
+            <option value="중구">중구</option>
+            <option value="중랑구">중랑구</option>
           </Select>
         </AddressDiv>
 
         <Input
           type="text"
-          required
           placeholder="상세 주소를 입력해주세요"
-          {...register('detailAddress')}
+          {...register('detailAddress', { required: true })}
         />
       </FormControl>
-      <FormControl>
+      <FormControl width={350} height={100}>
         <FormLabel>영업 시간</FormLabel>
         <Input
-          type="text"
-          required
-          placeholder="오픈 시간 HH:MM"
-          {...register('openTime')}
+          type="time"
+          {...register('openTime', { required: true })}
+          width={170}
+          marginRight={2}
         />
+
         <Input
-          type="text"
-          required
-          placeholder="마감 시간 HH:MM"
-          {...register('endTime')}
+          type="time"
+          {...register('endTime', { required: true })}
+          width={170}
         />
       </FormControl>
-      <FormControl>
+      <FormControl height={100} width={350}>
         <FormLabel>업체 설명</FormLabel>
-        <Input type="text" required {...register('description')} />
+        <Input type="text" {...register('description', { required: true })} />
       </FormControl>
-      <FormControl>
+      <FormControl height={100} width={350}>
         <FormLabel>사업자 등록증 사진</FormLabel>
-        <Input type="file" required {...register('businessLicenseImage')} />
+        <Input
+          type="file"
+          {...register('businessLicenseImage', { required: true })}
+          padding={1}
+        />
       </FormControl>
-      <FormControl>
+      <FormControl height={100} width={350}>
         <FormLabel>업체 대표 사진</FormLabel>
-        <Input type="file" required {...register('marketImage')} />
+        <Input
+          type="file"
+          {...register('marketImage', { required: true })}
+          padding={1}
+        />
       </FormControl>
-      <button type="submit">등록하기</button>
+      <Button
+        type="submit"
+        width={350}
+        padding={1}
+        bg="hey.lightOrange"
+        fontSize="1.3rem"
+        marginBottom={10}
+        _hover={{ bg: 'hey.sub' }}
+      >
+        등록하기
+      </Button>
     </Form>
   );
 }
@@ -131,4 +181,9 @@ const Form = styled.form`
 
 const AddressDiv = styled.div`
   display: flex;
+`;
+
+const ErrorSpan = styled.span`
+  color: red;
+  font-size: 8px;
 `;
---
+++ constants/errorMessages.ts
@@ -0,0 +1,6 @@
+const ERROR_MESSAGES = {
+  CHECK_BUSINESS_NUMBER: '* 10자리 수를 올바르게 입력해주세요.',
+  CHECK_OWNER_NAME: '* 올바른 이름을 작성해주세요.',
+};
+
+export default ERROR_MESSAGES;
