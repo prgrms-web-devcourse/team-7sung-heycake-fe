e67b04704db85a90b4211d732fa06642a72ae14d
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-02-26 22:14:15 +0900

    refactor: 00구 상수화

--- components/Mypage/enrollmentForm.tsx
+++ components/Mypage/enrollmentForm.tsx
@@ -10,6 +10,7 @@ import styled from '@emotion/styled';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
 import ERROR_MESSAGES from '@/constants/errorMessages';
+import SEOUL_AREA from '@/constants/seoulArea';
 
 const { CHECK_BUSINESS_NUMBER, CHECK_OWNER_NAME } = ERROR_MESSAGES;
 
@@ -91,31 +92,11 @@ export default function EnrollmentForm() {
             width={170}
           >
             <option value="">구 선택</option>
-            <option value="강남구">강남구</option>
-            <option value="강동구">강동구</option>
-            <option value="강서구">강서구</option>
-            <option value="강북구">강북구</option>
-            <option value="관악구">관악구</option>
-            <option value="광진구">광진구</option>
-            <option value="구로구">구로구</option>
-            <option value="금천구">금천구</option>
-            <option value="노원구">노원구</option>
-            <option value="동대문구">동대문구</option>
-            <option value="도봉구">도봉구</option>
-            <option value="동작구">동작구</option>
-            <option value="마포구">마포구</option>
-            <option value="서대문구">서대문구</option>
-            <option value="성동구">성동구</option>
-            <option value="성북구">성북구</option>
-            <option value="서초구">서초구</option>
-            <option value="송파구">송파구</option>
-            <option value="영등포구">영등포구</option>
-            <option value="용산구">용산구</option>
-            <option value="양천구">양천구</option>
-            <option value="은평구">은평구</option>
-            <option value="종로구">종로구</option>
-            <option value="중구">중구</option>
-            <option value="중랑구">중랑구</option>
+            {SEOUL_AREA.map((area) => (
+              <option key={area} value={area}>
+                {area}
+              </option>
+            ))}
           </Select>
         </AddressDiv>
 
