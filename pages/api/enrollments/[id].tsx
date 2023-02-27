d1330bbf5e26f4a1981c2e0258a4b4116178f742
Sunghum Paik <zerosial@gmail.com>
2023-02-27 23:12:22 +0900

    feat: 코드리뷰 반영 수정

--- pages/api/enrollments/[id].tsx
+++ pages/api/enrollments/[id].tsx
@@ -6,6 +6,5 @@ export default async function getMarketDetail(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
-  // enrollmentsId : request.query (632463)
   return response.status(200).end(JSON.stringify(marketDetail));
 }
