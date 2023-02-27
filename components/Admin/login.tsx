9a8fa256dd00ec4a0e71644f2399f26b50e8313c
Sunghum Paik <zerosial@gmail.com>
2023-02-27 15:20:23 +0900

    feat: 안내와 경고창 삭제

--- components/Admin/login.tsx
+++ components/Admin/login.tsx
@@ -2,8 +2,6 @@ import {
   Button,
   Container,
   FormControl,
-  FormErrorMessage,
-  FormHelperText,
   FormLabel,
   Grid,
   Input,
@@ -13,8 +11,6 @@ import { FormEvent, useState } from 'react';
 export default function AdminLogin() {
   const [inputId, setInputId] = useState('');
   const [inputPassword, setInputPassword] = useState('');
-  const isErrorId = inputId === '';
-  const isErrorPassword = inputPassword === '';
 
   const onSubmitHandler = (e: FormEvent) => {
     e.preventDefault();
@@ -28,20 +24,15 @@ export default function AdminLogin() {
     <Container pt={4}>
       <form onSubmit={onSubmitHandler}>
         <Grid gap={6}>
-          <FormControl isRequired isInvalid={isErrorId}>
+          <FormControl>
             <FormLabel>Id</FormLabel>
             <Input
               placeholder="Id"
               value={inputId}
               onChange={(e) => setInputId(e.target.value)}
             />
-            {!isErrorId ? (
-              <FormHelperText>운영자 Id를 입력해주세요</FormHelperText>
-            ) : (
-              <FormErrorMessage>Id 가 필요합니다</FormErrorMessage>
-            )}
           </FormControl>
-          <FormControl isRequired isInvalid={isErrorPassword}>
+          <FormControl>
             <FormLabel>Password</FormLabel>
             <Input
               type="password"
@@ -49,11 +40,6 @@ export default function AdminLogin() {
               value={inputPassword}
               onChange={(e) => setInputPassword(e.target.value)}
             />
-            {!isErrorPassword ? (
-              <FormHelperText>운영자 비밀번호를 입력해주세요</FormHelperText>
-            ) : (
-              <FormErrorMessage>Password 가 필요합니다</FormErrorMessage>
-            )}
           </FormControl>
           <Button type="submit" colorScheme="heys">
             확인
