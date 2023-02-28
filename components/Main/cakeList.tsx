0beedcea531020174db2b2ded38c891caf03304c
Sunghum Paik <zerosial@gmail.com>
2023-02-28 18:59:36 +0900

    feat: 메인 route 주소 변경

--- components/Main/cakeList.tsx
+++ components/Main/cakeList.tsx
@@ -23,7 +23,7 @@ export default function CakeList({ category, location }: any) {
   return (
     <Grid padding={0} gap={4} flexDirection="column">
       {data?.map((item: any) => (
-        <Link key={item.orderId} href={`/orderlist/${item.orderId}`}>
+        <Link key={item.orderId} href={`/orders/${item.orderId}`}>
           <CakeItem
             orderId={item.orderId}
             title={item.title}
