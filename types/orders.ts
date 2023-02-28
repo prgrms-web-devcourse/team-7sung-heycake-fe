51780cb221ef81adb1536c6508f29d7034925b10
tooooo1 <wer4272@nate.com>
2023-02-28 17:32:31 +0900

    rename: GREEN_TEA

--- types/orders.ts
+++ types/orders.ts
@@ -16,7 +16,7 @@ export interface Order {
       | 'ETC';
     cakeSize: 'MINI' | 'NO_1' | 'NO_2' | 'ETC';
     cakeHeight: 'ONE_LAYER' | 'TWO_LAYER' | 'ETC';
-    breadFlavor: 'VANILLA' | 'CHOCO' | 'GEEN_TEA' | 'CARROT' | 'ETC';
+    breadFlavor: 'VANILLA' | 'CHOCO' | 'GREEN_TEA' | 'CARROT' | 'ETC';
     creamFlavor: 'WHIPPED_CREAM' | 'CREAM_CHEESE' | 'CHOCO' | 'OREO' | 'ETC';
     requirements: string;
   };
