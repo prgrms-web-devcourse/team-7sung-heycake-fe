f418560dbd8b620c34bcc5e7ff98a61b231ec0a4
Sunghum Paik <zerosial@gmail.com>
2023-03-02 00:34:07 +0900

    feat: 화면 수정

--- components/Main/cakeItem.tsx
+++ components/Main/cakeItem.tsx
@@ -5,70 +5,77 @@ import {
   CardBody,
   Divider,
   Flex,
+  Grid,
   Text,
 } from '@chakra-ui/react';
 import Image from 'next/image';
 
 export default function CakeItem({
-  title,
   category,
   cakeSize,
   image,
   price,
   status,
   visitTime,
+  title,
 }: any) {
   return (
     <Card
-      bgImage={status === 'RESERVED' ? '/images/completedCake.png' : ''}
+      bgImage={status !== 'NEW' ? '/images/completedCake.png' : ''}
       bgPosition="center"
     >
       <Card
         borderColor="hey.sub"
         borderWidth="2px"
         bgColor="orange.50"
-        direction="row"
         variant="outline"
-        alignItems="center"
-        justifyContent="space-between"
-        opacity={status === 'RESERVED' ? '0.4' : '1'}
+        opacity={status !== 'NEW' ? '0.4' : '1'}
       >
-        <Box p={2} borderRadius="12px">
-          <Image width={100} height={100} src={image} alt="Cake" />
-        </Box>
-        <CardBody px={2}>
-          <Flex padding={0} gap={1} flexDirection="column">
-            <Flex align="center" gap={4} justifyContent="space-between">
-              <Text whiteSpace="nowrap">{title}</Text>
-            </Flex>
-            <Divider borderColor="hey.main" />
-            <Flex align="center" gap={4} justifyContent="space-between">
-              <Text color="hey.main">카테고리</Text>
-              <Text fontSize="sm">{category}</Text>
-            </Flex>
-            <Divider borderColor="hey.main" />
-            <Flex align="center" gap={4} justifyContent="space-between">
-              <Text color="hey.main">케익 크기</Text>
-              <Text fontSize="sm">{cakeSize}</Text>
-            </Flex>
-            <Divider borderColor="hey.main" />
+        <Grid>
+          <Flex m={1} justifyContent="space-between">
+            <Text>{title}</Text>
+            <Badge bgColor="red.200" color="hey.red">
+              ~ {visitTime}
+            </Badge>
           </Flex>
-        </CardBody>
-        <Flex
-          flexDirection="column"
-          justifyContent="space-between"
-          alignItems="end"
-          w={24}
-          h={32}
-          p={2}
-        >
-          <Badge bgColor="red.200" color="hey.red">
-            ~ {visitTime}
-          </Badge>
-          <Badge bgColor="orange.100" colorScheme="red" color="hey.red">
-            ~ ₩ {price}
-          </Badge>
-        </Flex>
+          <Flex>
+            <Card m={2} width="100px" height="100px">
+              <Image fill src={image} alt="Cake" />
+            </Card>
+            <CardBody px={0}>
+              <Flex padding={0} gap={1} flexDirection="column">
+                <Flex align="center" gap={4} justifyContent="space-between">
+                  <Text color="hey.main" whiteSpace="nowrap">
+                    항목
+                  </Text>
+                  <Text fontSize="sm" whiteSpace="nowrap">
+                    {category}
+                  </Text>
+                </Flex>
+                <Divider borderColor="hey.main" />
+                <Flex align="center" gap={4} justifyContent="space-between">
+                  <Text color="hey.main" whiteSpace="nowrap">
+                    케익 크기
+                  </Text>
+                  <Text fontSize="sm" whiteSpace="nowrap">
+                    {cakeSize}
+                  </Text>
+                </Flex>
+                <Divider borderColor="hey.main" />
+              </Flex>
+            </CardBody>
+            <Flex flexDirection="column" h={30} p={2}>
+              <Badge
+                bgColor="orange.100"
+                colorScheme="red"
+                color="hey.red"
+                mt={20}
+              >
+                ~ ₩ {price}
+              </Badge>
+            </Flex>
+          </Flex>
+        </Grid>
       </Card>
     </Card>
   );
