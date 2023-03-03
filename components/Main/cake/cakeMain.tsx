55873ff94d5ae76287ec34690acb04a4c8d16d5c
Sunghum Paik <zerosial@gmail.com>
2023-03-03 15:27:15 +0900

    feat: 넓이에 따른 반응형 디자인으로 변경

--- components/Main/cake/cakeMain.tsx
+++ components/Main/cake/cakeMain.tsx
@@ -1,4 +1,12 @@
-import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 
 import { TAB_TABLE } from '../constants';
@@ -9,31 +17,33 @@ export default function CakeMain() {
   const [location, setLocation] = useState('강남구');
 
   return (
-    <Tabs colorScheme="heys" isLazy>
-      <Box
-        overflow="scroll"
-        sx={{
-          '::-webkit-scrollbar': {
-            display: 'none',
-          },
-        }}
-      >
-        <TabList w="max-content" alignItems="center" h="60px" p={2}>
-          <LocationSelectBox location={location} setLocation={setLocation} />
+    <Flex justifyContent="center">
+      <Tabs colorScheme="heys" isLazy minW="350px" w="max-content">
+        <Box
+          overflow="scroll"
+          sx={{
+            '::-webkit-scrollbar': {
+              display: 'none',
+            },
+          }}
+        >
+          <TabList w="max-content" alignItems="center" h="60px" p={2}>
+            <LocationSelectBox location={location} setLocation={setLocation} />
+            {TAB_TABLE.map((tab) => (
+              <Tab key={tab.label} h="60px">
+                {tab.label}
+              </Tab>
+            ))}
+          </TabList>
+        </Box>
+        <TabPanels>
           {TAB_TABLE.map((tab) => (
-            <Tab key={tab.label} h="60px">
-              {tab.label}
-            </Tab>
+            <TabPanel p={3} key={tab.label}>
+              <CakeList category={tab.category} location={location} />
+            </TabPanel>
           ))}
-        </TabList>
-      </Box>
-      <TabPanels>
-        {TAB_TABLE.map((tab) => (
-          <TabPanel p={3} key={tab.label}>
-            <CakeList category={tab.category} location={location} />
-          </TabPanel>
-        ))}
-      </TabPanels>
-    </Tabs>
+        </TabPanels>
+      </Tabs>
+    </Flex>
   );
 }
