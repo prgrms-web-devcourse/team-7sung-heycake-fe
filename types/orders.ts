8a3644ce0a7edf3d7bf3df999bd9cb826fe4e3d7
tooooo1 <wer4272@nate.com>
2023-03-05 17:06:41 +0900

    refactor: type 재정의

--- types/orders.ts
+++ types/orders.ts
@@ -3,28 +3,42 @@ export interface Order {
   memberId: number;
   title: string;
   region: string;
-  orderStatus: 'NEW' | 'RESERVED' | 'PAID';
+  orderStatus: OrderStatus;
   hopePrice: number;
   images: string[];
   visitDate: string;
   cakeInfo: {
-    cakeCategory:
-      | 'ALL'
-      | 'PHOTO'
-      | 'LETTERING'
-      | 'CHARACTER_IMAGE'
-      | 'CHARACTER_MODEL'
-      | 'ETC';
-    cakeSize: 'MINI' | 'NO_1' | 'NO_2' | 'ETC';
-    cakeHeight: 'ONE_LAYER' | 'TWO_LAYER' | 'ETC';
-    breadFlavor: 'VANILLA' | 'CHOCO' | 'GREEN_TEA' | 'CARROT' | 'ETC';
-    creamFlavor: 'WHIPPED_CREAM' | 'CREAM_CHEESE' | 'CHOCO' | 'OREO' | 'ETC';
+    cakeCategory: CakeCategory;
+    cakeSize: CakeSize;
+    cakeHeight: CakeHeight;
+    breadFlavor: BreadFlavor;
+    creamFlavor: CreamFlavor;
     requirements: string;
   };
   offerCount: number;
   createdAt: string;
   updatedAt: string;
 }
+
+export type OrderStatus = 'NEW' | 'RESERVED' | 'PAID';
+
+export type CakeCategory =
+  | 'ALL'
+  | 'PHOTO'
+  | 'LETTERING'
+  | 'CHARACTER_IMAGE'
+  | 'CHARACTER_MODEL'
+  | 'ETC';
+
+export type CakeSize = 'MINI' | 'NO_1' | 'NO_2' | 'ETC';
+export type CakeHeight = 'ONE_LAYER' | 'TWO_LAYER' | 'ETC';
+export type BreadFlavor = 'VANILLA' | 'CHOCO' | 'GREEN_TEA' | 'CARROT' | 'ETC';
+export type CreamFlavor =
+  | 'WHIPPED_CREAM'
+  | 'CREAM_CHEESE'
+  | 'CHOCO'
+  | 'OREO'
+  | 'ETC';
 
 export interface ThreadDto {
   offerId: number;
