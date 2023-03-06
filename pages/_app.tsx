2afff15256b74fd1267243b0e755a9edcd7c6522
tooooo1 <wer4272@nate.com>
2023-03-06 09:59:02 +0900

    feat: scrollbar  none

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/font.css';
+import '@/styles/style.css';
 
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
