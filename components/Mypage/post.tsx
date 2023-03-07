d3f1949915f3e363ab7aabf356b14d0184bdf49e
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-07 20:40:47 +0900

    feat: 실제 내 게시글 데이터 연동

--- components/Mypage/post.tsx
+++ components/Mypage/post.tsx
@@ -1,8 +1,16 @@
 import { Box, Container, Text } from '@chakra-ui/react';
-import styled from '@emotion/styled';
 import Image from 'next/image';
 
-export default function Post() {
+import { IMypagePost } from './types';
+
+export default function Post({
+  id,
+  imageUrl,
+  orderStatus,
+  region,
+  title,
+  visitTime,
+}: IMypagePost) {
   return (
     <Container
       bgColor="hey.lightOrange"
@@ -13,32 +21,28 @@ export default function Post() {
       borderRadius="10"
       boxShadow="5px 2px 2px grey"
     >
-      <Box w="70px" h="70px">
-        <Image
-          src="/images/prgms.png"
-          width={70}
-          height={70}
-          alt="케이크 이미지"
-        />
+      <Box w="70px" h="70px" id={id}>
+        <Image src={imageUrl} width={70} height={70} alt="케이크 이미지" />
       </Box>
       <Container margin="0 0 0 5px" border="0" padding="0">
-        <OrderRequestCount>+ 3</OrderRequestCount>
+        <Container
+          color="white"
+          borderRadius="1rem"
+          backgroundColor="hey.main"
+          padding="0 1rem"
+          width="fit-content"
+          display="flex"
+        >
+          {orderStatus}
+        </Container>
         <Container padding="0">
           <Text fontSize="1rem" fontWeight="bold">
-            딸기 케이크 만들어주세요!
+            {title}
           </Text>
-          <Text color="gray">서울 특별시 양천구 목동</Text>
+          <Text color="gray">{region}</Text>
+          <Text color="gray">{visitTime}</Text>
         </Container>
       </Container>
     </Container>
   );
 }
-
-const OrderRequestCount = styled.span`
-  color: white;
-  background-color: #e53e3e;
-  padding: 0 1rem;
-  padding-bottom: 1px;
-  border-radius: 1rem;
-  margin-bottom: 2px;
-`;
