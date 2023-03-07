e8c51e35f1283caa306da5e29b8cea11c08e2d0e
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-07 21:09:40 +0900

    feat: 게시글 컴포넌트 출력

--- pages/mypage/orderlist/index.tsx
+++ pages/mypage/orderlist/index.tsx
@@ -1,16 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Container } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
+import getOrderList from '@/components/Api/Order';
 import MypageTitle from '@/components/Mypage/mypageTitle';
 import Post from '@/components/Mypage/post';
 
 export default function Orderlist() {
+  const [orderList, setOrderList] = useState<any[]>([]);
+
+  const [cursorId, setCursorId] = useState(null);
+  const [pageSize, setPageSize] = useState(null);
+  const [orderStatus, setOrderStatus] = useState(null);
+
+  useEffect(() => {
+    async function saveOrderList() {
+      const response = await getOrderList({ cursorId, pageSize, orderStatus });
+      setOrderList(response);
+    }
+    saveOrderList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <MypageTitle title="내 주문 리스트" isSuccess={false} />
       <Container marginTop={10}>
-        <Post />
-        <Post />
-        <Post />
+        {orderList?.map((order) => (
+          <Post {...order} />
+        ))}
       </Container>
     </>
   );
