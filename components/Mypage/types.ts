d70527489362c42e90e1ca7cb5001fb5d3c5ecf3
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-07 20:39:09 +0900

    feat: 게시글 interface 추가

--- components/Mypage/types.ts
+++ components/Mypage/types.ts
@@ -2,3 +2,12 @@ export interface IMypageTitle {
   isSuccess: boolean;
   title: string;
 }
+
+export interface IMypagePost {
+  id: string;
+  imageUrl: string;
+  orderStatus: string;
+  region: string;
+  title: string;
+  visitTime: string;
+}
