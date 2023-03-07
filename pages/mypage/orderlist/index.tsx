9cbcd5a348aeb05bc8b10864952ec8dba616efe7
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-08 01:18:23 +0900

    feat: 주문 리스트가 존재하지 않을 때 화면 구현

--- pages/mypage/orderlist/index.tsx
+++ pages/mypage/orderlist/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Container } from '@chakra-ui/react';
+import { Button, Container, Text } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 import getOrderList from '@/components/Api/Order';
@@ -12,6 +13,8 @@ export default function Orderlist() {
   const [cursorId, setCursorId] = useState(null);
   const [pageSize, setPageSize] = useState(null);
   const [orderStatus, setOrderStatus] = useState(null);
+
+  const router = useRouter();
 
   useEffect(() => {
     async function saveOrderList() {
@@ -26,9 +29,26 @@ export default function Orderlist() {
     <>
       <MypageTitle title="내 주문 리스트" isSuccess={false} />
       <Container paddingTop={10} overflow="scroll">
-        {orderList?.map((order) => (
-          <Post {...order} />
-        ))}
+        {orderList.length !== 0 ? (
+          orderList?.map((order) => <Post {...order} />)
+        ) : (
+          <Container display="flex" flexDir="column" alignItems="center">
+            <Text fontSize="1.2rem" fontWeight="bold" margin="12rem 0 2rem 0">
+              아직 주문이 존재하지 않아요!
+            </Text>
+            <Button
+              fontSize="1.2rem"
+              fontWeight="bold"
+              marginTop="2rem"
+              backgroundColor="hey.lightOrange"
+              width="20rem"
+              _hover={{ bg: 'hey.sub' }}
+              onClick={() => router.push('/orders/new')}
+            >
+              게시글 등록하러 가기
+            </Button>
+          </Container>
+        )}
       </Container>
     </>
   );
