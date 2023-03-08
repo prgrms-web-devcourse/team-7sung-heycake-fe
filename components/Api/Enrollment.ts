41d8e7bd55a8525576f66e4946e11e539fa15b14
Sunghum Paik <zerosial@gmail.com>
2023-03-08 17:10:03 +0900

    feat: 지연님 수정사항 반영

--- components/Api/Enrollment.ts
+++ components/Api/Enrollment.ts
@@ -1,9 +1,18 @@
+import { getAccessToken } from '@/utils/getAccessToken';
+
 import { publicApi } from '.';
 
+const ACCESS_TOKEN = getAccessToken();
+
 export default async function postEnrollment(data: object) {
+  if (ACCESS_TOKEN === null) {
+    alert('로그인을 해주세요');
+    return;
+  }
   try {
     await publicApi.post('/enrollments', data, {
       headers: {
+        access_token: ACCESS_TOKEN,
         'Content-Type': 'multipart/form-data',
       },
     });
