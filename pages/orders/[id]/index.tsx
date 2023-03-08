f12084e0087ce6a0a2b199ed76865409db3cc59f
tooooo1 <wer4272@nate.com>
2023-03-08 11:19:17 +0900

    fix: 이미지 짤림 현상 및 이미지 변경 버튼 수정

--- pages/orders/[id]/index.tsx
+++ pages/orders/[id]/index.tsx
@@ -23,85 +23,87 @@ interface OrdersProps {
 
 export default function Orders({ order, threads, orderId }: OrdersProps) {
   return (
-    <Flex
-      flexDirection="column"
-      style={{ width: '100%', gap: '1rem', padding: '1rem' }}
-    >
+    <>
       <ImageSlider images={order.images} />
-      <div>
-        <Tag
-          style={{
-            color: 'white',
-            backgroundColor: '#e53e3e',
-            padding: '0 1rem',
-            paddingBottom: '2px',
-            borderRadius: '1rem',
-          }}
-        >
-          {getOrderStatusText(order.orderStatus, order.offerCount)}
-        </Tag>
-        <Heading as="h1" size="xl">
-          {order.title}
-        </Heading>
-        <Box fontSize="0.9rem" color="#777777">
-          {order.region}
-        </Box>
-      </div>
-      <DataTable
-        title="케익 맛"
-        value={convertBreadFlavor(order.cakeInfo.breadFlavor)}
-      />
-      <DataTable
-        title="케익 카테고리"
-        value={convertCakeCategory(order.cakeInfo.cakeCategory)}
-      />
-      <DataTable
-        title="케익 높이"
-        value={convertCakeHeight(order.cakeInfo.cakeHeight)}
-      />
-      <DataTable
-        title="케익 사이즈"
-        value={convertCakeSize(order.cakeInfo.cakeSize)}
-      />
-      <DataTable
-        title="케익 크림 맛"
-        value={convertCreamFlavor(order.cakeInfo.creamFlavor)}
-      />
-      <Box padding="2rem 0" fontSize="0.8rem">
-        {order.cakeInfo.requirements}
-      </Box>
       <Flex
-        justifyContent="space-between"
-        bg="#feebcb"
-        padding="1.4rem"
-        alignItems="center"
-        fontWeight="bold"
-        borderRadius="6px"
+        flexDirection="column"
+        style={{ width: '100%', gap: '1rem', padding: '1rem' }}
       >
-        <Image
-          alt="birthday-cake"
-          src="/images/birthday-cake.png"
-          width={40}
-          height={40}
+        <div>
+          <Tag
+            style={{
+              color: 'white',
+              backgroundColor: '#e53e3e',
+              padding: '0 1rem',
+              paddingBottom: '2px',
+              borderRadius: '1rem',
+            }}
+          >
+            {getOrderStatusText(order.orderStatus, order.offerCount)}
+          </Tag>
+          <Heading as="h1" size="xl">
+            {order.title}
+          </Heading>
+          <Box fontSize="0.9rem" color="#777777">
+            {order.region}
+          </Box>
+        </div>
+        <DataTable
+          title="케익 맛"
+          value={convertBreadFlavor(order.cakeInfo.breadFlavor)}
         />
-        신청한 케이크 업체 {order.offerCount}개
-        <Link
-          style={{
-            backgroundColor: '#f96400',
-            color: 'white',
-            padding: '10px',
-            borderRadius: '5px',
-          }}
-          href="/orders/[orderId]/new-offer"
-          as={`/orders/${orderId}/new-offer`}
+        <DataTable
+          title="케익 카테고리"
+          value={convertCakeCategory(order.cakeInfo.cakeCategory)}
+        />
+        <DataTable
+          title="케익 높이"
+          value={convertCakeHeight(order.cakeInfo.cakeHeight)}
+        />
+        <DataTable
+          title="케익 사이즈"
+          value={convertCakeSize(order.cakeInfo.cakeSize)}
+        />
+        <DataTable
+          title="케익 크림 맛"
+          value={convertCreamFlavor(order.cakeInfo.creamFlavor)}
+        />
+        <Box padding="2rem 0" fontSize="0.8rem">
+          {order.cakeInfo.requirements}
+        </Box>
+        <Flex
+          justifyContent="space-between"
+          bg="#feebcb"
+          padding="1.4rem"
+          alignItems="center"
+          fontWeight="bold"
+          borderRadius="6px"
         >
-          신청하기
-        </Link>
+          <Image
+            alt="birthday-cake"
+            src="/images/birthday-cake.png"
+            width={40}
+            height={40}
+          />
+          신청한 케이크 업체 {order.offerCount}개
+          <Link
+            style={{
+              backgroundColor: '#f96400',
+              color: 'white',
+              padding: '10px',
+              borderRadius: '5px',
+            }}
+            href="/orders/[orderId]/new-offer"
+            as={`/orders/${orderId}/new-offer`}
+          >
+            신청하기
+          </Link>
+        </Flex>
+        {threads?.map((thread) => (
+          <Thread key={thread.offerId} thread={thread} orderId={orderId} />
+        ))}
       </Flex>
-      {threads?.map((thread) => (
-        <Thread key={thread.offerId} thread={thread} orderId={orderId} />
-      ))}
-    </Flex>
+    </>
   );
 }
 
