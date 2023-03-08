bce378d65647a09bdd7b464fe53a915548cda7ce
Sunghum Paik <zerosial@gmail.com>
2023-03-08 17:24:34 +0900

    feat: staticPaths와 props를 추가

--- pages/market/[id].tsx
+++ pages/market/[id].tsx
@@ -1,5 +1,33 @@
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+import { getMarketDetail } from '@/components/Api/Market';
 import MarketProfile from '@/components/Market';
 
 export default function Market() {
   return <MarketProfile />;
 }
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: [{ params: { id: 1 } }],
+  fallback: true,
+});
+
+export const getStaticProps: GetStaticProps = async (params) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    ['업체 상세 정보', params.id],
+    () => getMarketDetail({ enrollmentId: String(params.id) }),
+    {
+      staleTime: 20000,
+    }
+  );
+
+  return {
+    props: {
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+      revalidate: 86400,
+    },
+  };
+};
