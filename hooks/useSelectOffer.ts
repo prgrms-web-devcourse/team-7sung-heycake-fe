327ef1df5caea80bd6d0582974374fe3dde4f707
tooooo1 <wer4272@nate.com>
2023-03-08 21:50:40 +0900

    fix: selectoffer

--- hooks/useSelectOffer.ts
+++ hooks/useSelectOffer.ts
@@ -3,6 +3,7 @@ import { AxiosError } from 'axios';
 import { useCallback } from 'react';
 
 import { publicApi } from '@/components/Api';
+import { getAccessToken } from '@/utils/getAccessToken';
 
 interface RequestBody {
   orderId: string;
@@ -14,8 +15,15 @@ interface ErrorResponse {
 }
 
 const useSelectOffer = () => {
+  const accessToken = getAccessToken();
+
   const mutation = useMutation(
-    (body: RequestBody) => publicApi.post('/histories', body),
+    (body: RequestBody) =>
+      publicApi.post('/histories', body, {
+        headers: {
+          access_token: accessToken as string,
+        },
+      }),
     {
       onSuccess: () => {
         alert('해당 업체를 선택하셨어요');
@@ -28,11 +36,13 @@ const useSelectOffer = () => {
           axiosError.response.data.message ?? '{}'
         ) as { message?: string };
 
-        if (axiosError.response.status === 401) {
+        if (axiosError.response.status === 400) {
+          alert(errorMessage.message || '업체 선택이 실패했습니다.');
+        } else if (axiosError.response.status === 401) {
           alert(errorMessage.message || '인증되지 않은 요청입니다.');
-        } else if (axiosError.response?.status === 403) {
+        } else if (axiosError.response.status === 403) {
           alert(errorMessage.message || '접근 권한이 없습니다.');
-        } else if (axiosError.response?.status === 500) {
+        } else if (axiosError.response.status === 500) {
           alert(
             errorMessage.message ||
               '서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.'
@@ -49,9 +59,13 @@ const useSelectOffer = () => {
   const selectOffer = useCallback(
     (orderId: string, offerId: number) => {
       const requestBody = { orderId, offerId };
-      mutation.mutate(requestBody);
+      if (accessToken) {
+        mutation.mutate(requestBody);
+      } else {
+        alert('로그인을 해주세요');
+      }
     },
-    [mutation]
+    [accessToken, mutation]
   );
 
   return selectOffer;
