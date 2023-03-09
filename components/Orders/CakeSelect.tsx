740aefa8f469b5759abedfb683b04031a1dc0d3d
tooooo1 <wer4272@nate.com>
2023-03-09 18:13:21 +0900

    style: cakeselectCake

--- components/Orders/CakeSelect.tsx
+++ components/Orders/CakeSelect.tsx
@@ -20,7 +20,29 @@ function CakeSelect<T>({
   return (
     <FormControl id={id}>
       <FormLabel>{label}</FormLabel>
-      <Select name={id} value={value} onChange={onChange}>
+      <Select
+        icon={
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14.625 6.75L9 12.375L3.375 6.75"
+              stroke="#292929"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        }
+        borderRadius="1rem"
+        height="3rem"
+        name={id}
+        value={value}
+        onChange={onChange}
+      >
         {options.map((option) => (
           <option key={option as string} value={option as string}>
             {convertOption(option)}
