ef29b9e97722a762af648f01d2ffb5289a87a506
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-09 18:55:28 +0900

    design: 마이페이지 첫 진입 화면 스타일링

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,62 +1,82 @@
-import { Button } from '@chakra-ui/react';
-import styled from '@emotion/styled';
-import { useRouter } from 'next/router';
+import { Box, Container, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Detail() {
-  const router = useRouter();
-
   return (
-    <MypageContainer>
-      <MessageContainer>
-        <strong>회원님</strong>
+    <Container
+      display="flex"
+      flexDirection="column"
+      flexDir="column"
+      padding="150px 0 0 30px"
+    >
+      <Text display="inline" fontSize={30} fontWeight="bold">
+        반갑습니다
         <br />
-        환영합니다!
-      </MessageContainer>
-      <MypageButtonContainer>
-        <Button
-          bg="hey.lightOrange"
-          width="20rem"
-          height="3rem"
-          margin="1.5rem"
-          fontSize="1.3rem"
-          _hover={{ bg: 'hey.sub' }}
-          onClick={() => router.push('/mypage/orderlist')}
-        >
-          내 주문 리스트
-        </Button>
-        <Button
-          bg="hey.lightOrange"
-          width="20rem"
-          height="3rem"
-          fontSize="1.3rem"
-          _hover={{ bg: 'hey.sub' }}
-          onClick={() => router.push('/mypage/enrollment')}
-        >
-          업체 정보 등록
-        </Button>
-      </MypageButtonContainer>
-    </MypageContainer>
+        <Text display="inline" color="hey.main">
+          헤이, 케이크
+        </Text>
+        입니다!
+      </Text>
+      <Container display="flex" marginTop={20} padding={0}>
+        <Link href="/mypage/orderlist">
+          <Box
+            display="flex"
+            flexDir="column"
+            alignContent="center"
+            border="1px solid"
+            borderColor="hey.lightGray"
+            width="154px"
+            height="140px"
+            padding={5}
+            borderRadius={10}
+            _hover={{ borderColor: 'hey.main' }}
+          >
+            <Container>
+              <Image
+                src="/images/cakeIcon.png"
+                width={66}
+                height={66}
+                quality={100}
+                alt="주문 리스트 아이콘"
+              />
+            </Container>
+
+            <Text fontSize={16}>내 주문 리스트</Text>
+          </Box>
+        </Link>
+        <Link href="/mypage/enrollment">
+          <Box
+            display="flex"
+            flexDir="column"
+            border="1px solid"
+            borderColor="hey.lightGray"
+            width="154px"
+            height="140px"
+            padding={5}
+            borderRadius={10}
+            marginLeft={5}
+            _hover={{ borderColor: 'hey.main' }}
+          >
+            <Container>
+              <Image
+                src="/images/shop.png"
+                width={66}
+                height={66}
+                quality={100}
+                alt="업체 등록 아이콘"
+              />
+            </Container>
+
+            <Text fontSize={16}>업체 정보 등록</Text>
+          </Box>
+        </Link>
+      </Container>
+      <Container>
+        <Text fontSize={20} color="hey.lightGray">
+          로그아웃
+        </Text>
+      </Container>
+    </Container>
   );
 }
-
-const MypageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const MessageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 11rem;
-  font-size: 1.4rem;
-  line-height: 70%;
-`;
-
-const MypageButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 10rem;
-`;
