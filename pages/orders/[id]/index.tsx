22218702ebaf823622e8db40c78abb95e788c194
tooooo1 <wer4272@nate.com>
2023-03-10 15:02:09 +0900

    refactor: design order page

--- pages/orders/[id]/index.tsx
+++ pages/orders/[id]/index.tsx
@@ -1,18 +1,16 @@
-import { Box, Flex, Heading, Tag } from '@chakra-ui/react';
+import { Badge, Box, Flex } from '@chakra-ui/react';
 import { GetServerSideProps } from 'next';
-import Image from 'next/image';
-import Link from 'next/link';
 
 import { publicApi } from '@/components/Api';
 import { DataTable, ImageSlider, Thread } from '@/components/Orders';
+import { CAKE_CATEGORY } from '@/constants/Main';
 import { Order, ThreadDto } from '@/types/orders';
+import numberWithCommas from '@/utils/numberWithCommas';
 import {
   convertBreadFlavor,
-  convertCakeCategory,
   convertCakeHeight,
   convertCakeSize,
   convertCreamFlavor,
-  getOrderStatusText,
 } from '@/utils/orders';
 
 interface OrdersProps {
@@ -25,79 +23,57 @@ export default function Orders({ order, threads, orderId }: OrdersProps) {
   return (
     <>
       <ImageSlider images={order.images} />
-      <Flex
-        flexDirection="column"
-        style={{ width: '100%', gap: '1rem', padding: '1rem' }}
-      >
-        <div>
-          <Tag
-            style={{
-              color: 'white',
-              backgroundColor: '#e53e3e',
-              padding: '0 1rem',
-              paddingBottom: '2px',
-              borderRadius: '1rem',
-            }}
+      <Flex flexDirection="column" width="100%" gap="1rem" padding="1rem">
+        <Flex flexDirection="column" alignItems="center" gap="0.1rem">
+          <Badge
+            colorScheme={order.cakeInfo.cakeCategory}
+            p={1}
+            px={2}
+            borderRadius={10}
+            fontWeight={500}
+            fontSize="10px"
           >
-            {getOrderStatusText(order.orderStatus, order.offerCount)}
-          </Tag>
-          <Heading as="h1" size="xl">
+            {CAKE_CATEGORY[order.cakeInfo.cakeCategory]}
+          </Badge>
+          <Box fontSize="2rem" fontWeight="600">
             {order.title}
-          </Heading>
-          <Box fontSize="0.9rem" color="#777777">
+          </Box>
+          <Box fontSize="1.2rem" color="#777777">
             {order.region}
           </Box>
-        </div>
-        <DataTable
-          title="케익 맛"
-          value={convertBreadFlavor(order.cakeInfo.breadFlavor)}
-        />
-        <DataTable
-          title="케익 카테고리"
-          value={convertCakeCategory(order.cakeInfo.cakeCategory)}
-        />
-        <DataTable
-          title="케익 높이"
-          value={convertCakeHeight(order.cakeInfo.cakeHeight)}
-        />
-        <DataTable
-          title="케익 사이즈"
-          value={convertCakeSize(order.cakeInfo.cakeSize)}
-        />
-        <DataTable
-          title="케익 크림 맛"
-          value={convertCreamFlavor(order.cakeInfo.creamFlavor)}
-        />
-        <Box padding="2rem 0" fontSize="0.8rem">
-          {order.cakeInfo.requirements}
+        </Flex>
+        <Box fontSize="1.4rem" fontWeight="700" paddingTop="1rem">
+          주문 요약
         </Box>
-        <Flex
-          justifyContent="space-between"
-          bg="#feebcb"
-          padding="1.4rem"
-          alignItems="center"
-          fontWeight="bold"
-          borderRadius="6px"
-        >
-          <Image
-            alt="birthday-cake"
-            src="/images/birthday-cake.png"
-            width={40}
-            height={40}
+        <Box bg="#f8f8f8" padding="2rem" borderRadius="1rem">
+          <DataTable
+            title="희망 가격"
+            value={`~ ${numberWithCommas(order.hopePrice)}원`}
           />
-          신청한 케이크 업체 {order.offerCount}개
-          <Link
-            style={{
-              backgroundColor: '#f96400',
-              color: 'white',
-              padding: '10px',
-              borderRadius: '5px',
-            }}
-            href="/orders/[orderId]/new-offer"
-            as={`/orders/${orderId}/new-offer`}
-          >
-            신청하기
-          </Link>
+          <DataTable title="방문 예정 시간" value={order.visitDate} />
+          <DataTable
+            title="케이크 맛"
+            value={convertBreadFlavor(order.cakeInfo.breadFlavor)}
+          />
+          <DataTable
+            title="케이크 높이"
+            value={convertCakeHeight(order.cakeInfo.cakeHeight)}
+          />
+          <DataTable
+            title="케이크 크기"
+            value={convertCakeSize(order.cakeInfo.cakeSize)}
+          />
+          <DataTable
+            title="크림 맛"
+            value={convertCreamFlavor(order.cakeInfo.creamFlavor)}
+          />
+        </Box>
+        <Box fontSize="1.4rem" fontWeight="700" paddingTop="1rem">
+          요청사항
+        </Box>
+        <Box padding="1rem 0">{order.cakeInfo.requirements}</Box>
+        <Flex fontSize="1.4rem" fontWeight="700" gap="0.5rem" paddingTop="1rem">
+          신청한 케이크 업체 <Box color="hey.main">{order.offerCount}</Box>
         </Flex>
         {threads?.map((thread) => (
           <Thread key={thread.offerId} thread={thread} orderId={orderId} />
