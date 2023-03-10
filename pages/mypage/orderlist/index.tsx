e17a8236833e447dfff7dde1617a855a06b14a23
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-10 17:35:09 +0900

    refactor: 불필요한 코드 제거

--- pages/mypage/orderlist/index.tsx
+++ pages/mypage/orderlist/index.tsx
@@ -1,17 +1,15 @@
-import { Box, Button, Container, Text } from '@chakra-ui/react';
+import { Box, Container } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
-import { useRouter } from 'next/router';
 
 import { getOrderList } from '@/components/Api/Order';
 import CountBar from '@/components/Mypage/countBar';
 import FilterBar from '@/components/Mypage/filterBar';
 import MypageTitle from '@/components/Mypage/mypageTitle';
+import NotExist from '@/components/Mypage/notExist';
 import Post from '@/components/Mypage/post';
 import { IMypagePost } from '@/components/Mypage/types';
 
 export default function Orderlist() {
-  const router = useRouter();
-
   const { data: orderList, isError } = useQuery<IMypagePost[]>(
     ['orderList'],
     () => getOrderList({ cursorId: null, pageSize: null, orderStatus: null })
@@ -34,22 +32,7 @@ export default function Orderlist() {
         {orderList.length !== 0 ? (
           orderList.map((order) => <Post key={order.id} {...order} />)
         ) : (
-          <Container display="flex" flexDir="column" alignItems="center">
-            <Text fontSize="1.2rem" fontWeight="bold" margin="12rem 0 2rem 0">
-              아직 주문이 존재하지 않아요!
-            </Text>
-            <Button
-              fontSize="1.2rem"
-              fontWeight="bold"
-              marginTop="2rem"
-              backgroundColor="hey.lightOrange"
-              width="20rem"
-              _hover={{ bg: 'hey.sub' }}
-              onClick={() => router.push('/orders/new')}
-            >
-              주문 등록하러 가기
-            </Button>
-          </Container>
+          <NotExist />
         )}
       </Container>
     </>
