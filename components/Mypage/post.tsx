0fe1206ec8bade766991d2808e9f1446ee37e74a
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-11 17:46:58 +0900

    design: 주문 리스트 디자인 적용

--- components/Mypage/post.tsx
+++ components/Mypage/post.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Container, Text, useToast } from '@chakra-ui/react';
+import { Badge, Button, Container, Text, useToast } from '@chakra-ui/react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -56,46 +56,84 @@ export default function Post({
 
   return (
     <Container
-      marginBottom="1rem"
+      marginBottom="4rem"
       key={id}
       onClick={() => router.push(`/orders/${id}`)}
+      height={140}
+      width={360}
+      padding={0}
     >
-      <Text>
-        주문번호 {createdAt.slice(0, 10).replaceAll('-', '').concat(id)}
-      </Text>
-      <Image
-        src={imageUrl}
-        width={70}
-        height={70}
-        quality={100}
-        alt="케이크 이미지"
-      />
-      <Container>
-        <Card width={20}>{convertCakeCategory(cakeInfo.cakeCategory)}</Card>
-        <Text>{title}</Text>
-        <Text>
-          {convertCakeSize(cakeInfo.cakeSize)}&nbsp;|&nbsp;
-          {convertCakeHeight(cakeInfo.cakeHeight)}&nbsp;|&nbsp;
-          {convertCreamFlavor(cakeInfo.creamFlavor)}
+      <Container display="flex" justifyContent="space-between">
+        <Text fontSize={18} fontWeight="semibold">
+          주문번호 {createdAt.slice(0, 10).replaceAll('-', '').concat(id)}
         </Text>
-        <Text>~{hopePrice}원</Text>
-        <Container display="flex">
+        <Button
+          onClick={(e) => handleDelete(e)}
+          fontSize={12}
+          background="none"
+          color="hey.normalGray"
+        >
+          삭제
+        </Button>
+      </Container>
+      <Container display="flex" marginTop={2}>
+        <Container width="240px" height="150px" borderRadius="7px" padding={0}>
           <Image
-            src="/images/CalenderIcon.png"
-            width={20}
-            height={10}
-            alt="캘린더 아이콘"
+            src={imageUrl}
+            width={120}
+            height={120}
+            quality={100}
+            alt="케이크 이미지"
           />
-          {visitTime.slice(0, 10).replaceAll('-', '.')}
-          <Text>
-            <BsDot />
-            {getOrderStatusText(orderStatus, count) !==
-            ('선택 완료' || '거래 완료' || '알 수 없는 상태')
-              ? `오퍼 ${getOrderStatusText(orderStatus, count)}`
-              : getOrderStatusText(orderStatus, count)}
-          </Text>
         </Container>
-        <Button onClick={(e) => handleDelete(e)}>X</Button>
+        <Container>
+          <Badge
+            width={12}
+            borderRadius="6px"
+            display="flex"
+            justifyContent="center"
+            fontSize={10}
+            marginBottom={1}
+          >
+            {convertCakeCategory(cakeInfo.cakeCategory)}
+          </Badge>
+          <Text fontSize={14} marginBottom={1}>
+            {title}
+          </Text>
+          <Text
+            fontSize={12}
+            color="hey.normalGray"
+            display="flex"
+            marginBottom={1}
+          >
+            {convertCakeSize(cakeInfo.cakeSize)}
+            <BsDot />
+            {convertCakeHeight(cakeInfo.cakeHeight)}
+            <BsDot />
+            {convertCreamFlavor(cakeInfo.creamFlavor)}
+          </Text>
+          <Text fontSize={14} fontWeight="bold" marginBottom={1}>
+            ~{hopePrice}원
+          </Text>
+          <Container display="flex" marginBottom={1} padding={0}>
+            <Image
+              src="/images/CalenderIcon.png"
+              width={20}
+              height={10}
+              alt="캘린더 아이콘"
+            />
+            <Text color="hey.normalGray" fontSize={12} display="flex">
+              {visitTime.slice(0, 10).replaceAll('-', '.')}
+              <BsDot />
+            </Text>
+            <Text color="hey.main" fontSize={12}>
+              {getOrderStatusText(orderStatus, count) !==
+              ('선택 완료' || '거래 완료' || '알 수 없는 상태')
+                ? `오퍼 ${getOrderStatusText(orderStatus, count)}`
+                : getOrderStatusText(orderStatus, count)}
+            </Text>
+          </Container>
+        </Container>
       </Container>
     </Container>
   );
