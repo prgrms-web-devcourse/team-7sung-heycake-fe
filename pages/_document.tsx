c1faec86ab74c5b13f58fed9a612b2c6672b233e
Sunghum Paik <zerosial@gmail.com>
2023-03-12 01:09:04 +0900

    refactor: 카카오 상수 분리

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,9 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
+import KAKAO_MAP_URL from '@/constants/KakaoConstants';
+
 export default function Document() {
-  const KAKAO_MAP_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`;
   return (
     <Html lang="ko">
       <Head />
