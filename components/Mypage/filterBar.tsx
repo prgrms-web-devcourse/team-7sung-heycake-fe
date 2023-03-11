bd10b1c7ee4963a2d37ee4d012c3dea201e42776
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-12 01:19:07 +0900

    feat: 마이 주문 리스트 default 클릭 필터 적용

--- components/Mypage/filterBar.tsx
+++ components/Mypage/filterBar.tsx
@@ -3,9 +3,10 @@ import { Dispatch, SetStateAction } from 'react';
 
 interface SetStatus {
   setStatusFun: Dispatch<SetStateAction<string>>;
+  status: string;
 }
 
-export default function FilterBar({ setStatusFun }: SetStatus) {
+export default function FilterBar({ setStatusFun, status }: SetStatus) {
   return (
     <Container
       display="flex"
@@ -22,7 +23,8 @@ export default function FilterBar({ setStatusFun }: SetStatus) {
         width={40}
         fontSize="14px"
         padding={2}
-        _hover={{ borderBottom: '3px solid orange', color: 'hey.main' }}
+        borderBottom={status === 'NEW' ? '3px solid orange' : 'none'}
+        color={status === 'NEW' ? 'hey.main' : 'black'}
         onClick={() => setStatusFun('NEW')}
       >
         진행 중 주문
@@ -35,7 +37,8 @@ export default function FilterBar({ setStatusFun }: SetStatus) {
         width={40}
         padding={2}
         fontSize="14px"
-        _hover={{ borderBottom: '3px solid orange', color: 'hey.main' }}
+        borderBottom={status === 'PAID' ? '3px solid orange' : 'none'}
+        color={status === 'PAID' ? 'hey.main' : 'black'}
         onClick={() => setStatusFun('PAID')}
       >
         진행 완료 주문
