947bd6481cff2f625de727f76ab43fd40f882b8d
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-03-13 13:12:24 +0900

    feat: 모달 커스텀 기능 추가

--- components/Mypage/successModal.tsx
+++ components/Mypage/successModal.tsx
@@ -3,52 +3,77 @@ import {
   Container,
   Modal,
   ModalBody,
-  ModalCloseButton,
   ModalContent,
+  ModalFooter,
   ModalHeader,
   ModalOverlay,
   Text,
-  useDisclosure,
+  useToast,
 } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
-export default function SuccessModal() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+import deleteAccessToken from '@/utils/deleteAccessToken';
+
+interface SuccessInfo {
+  success: boolean;
+  marketName: string;
+  ownerName: string;
+  address: string;
+}
+
+export default function SuccessModal({ ...props }: SuccessInfo) {
+  const router = useRouter();
+  const toast = useToast();
+  const [open, setOpen] = useState(props.success);
+
+  function handleSuccess() {
+    toast({
+      status: 'success',
+      description: '업체 등록이 성공적으로 신청되었어요. 다시 로그인 해주세요.',
+      isClosable: true,
+    });
+    deleteAccessToken();
+    router.push('/');
+  }
   return (
-    <>
-      <Button onClick={onOpen}>Open Modal</Button>
-
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent width={400} height={600} borderRadius="14px">
-          <ModalHeader marginTop={10}>
-            <Text fontSize={28}>업체 등록이</Text>
-            <Text fontSize={28} color="hey.main">
-              완료되었습니다.
+    <Modal isOpen={open} onClose={() => setOpen(false)} closeOnOverlayClick>
+      <ModalOverlay />
+      <ModalContent width={400} height={600} borderRadius="14px">
+        <ModalHeader marginTop={8}>
+          <Text fontSize={30} fontWeight="medium">
+            업체 등록이
+          </Text>
+          <Text fontSize={30} color="hey.main" fontWeight="medium">
+            완료되었습니다.
+          </Text>
+        </ModalHeader>
+        <ModalBody marginTop={10}>
+          <Container>
+            <Text fontWeight="bold" fontSize={18}>
+              상호명
             </Text>
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody marginTop={10}>
-            <Container>
-              <Text fontWeight="bold" fontSize={18}>
-                상호명
-              </Text>
-            </Container>
-            <Container>
-              <Text fontWeight="bold" fontSize={18}>
-                대표자 이름
-              </Text>
-            </Container>
-            <Container>
-              <Text fontWeight="bold" fontSize={18}>
-                주소
-              </Text>
-            </Container>
-          </ModalBody>
+            <Text>{props.marketName}</Text>
+          </Container>
+          <Container>
+            <Text fontWeight="bold" fontSize={18}>
+              대표자 이름
+            </Text>
+            <Text>{props.ownerName}</Text>
+          </Container>
+          <Container>
+            <Text fontWeight="bold" fontSize={18}>
+              주소
+            </Text>
+            <Text>{props.address}</Text>
+          </Container>
+        </ModalBody>
 
-          <Container marginBottom={8} display="flex" justifyContent="center">
+        <Container marginBottom={8} display="flex" justifyContent="center">
+          <ModalFooter>
             <Button
               variant="ghost"
-              onClick={onClose}
+              onClick={() => setOpen(false)}
               border="1px"
               borderRadius="12px"
               width="10rem"
@@ -64,12 +89,13 @@ export default function SuccessModal() {
               height="3rem"
               mr={3}
               borderRadius="12px"
+              onClick={() => handleSuccess()}
             >
               맞습니다
             </Button>
-          </Container>
-        </ModalContent>
-      </Modal>
-    </>
+          </ModalFooter>
+        </Container>
+      </ModalContent>
+    </Modal>
   );
 }
